61d3d5a80f262856d34c73a36862f1391d770e0d
Clinc8686 <mario1.lanpert@gmail.com>
2022-05-03 21:55:58 +0200

    changed Button

--- SayHello/src/main/java/de/hochschule/trier/ema/StartActivity.java
+++ SayHello/src/main/java/de/hochschule/trier/ema/StartActivity.java
@@ -2,7 +2,9 @@ package de.hochschule.trier.ema;
 
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,31 +13,29 @@ import java.util.Locale;
 
 public class StartActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private TextToSpeech tts;
+    private EditText TextZuSprechen;
 
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitiy_start);
+        TextZuSprechen = (EditText) findViewById(R.id.editTextZuSprechen);
 
         tts = new TextToSpeech(this, this);
-        findViewById(R.id.btnVorlesen).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText TextZuSprechen = (EditText) findViewById(R.id.editTextZuSprechen);
-                if (TextZuSprechen.getText().length() > 0) {
-                    tts.speak(TextZuSprechen.getText(), TextToSpeech.QUEUE_FLUSH, null, "Test");
-                } else {
-                    tts.speak("Gib einen Text ein", TextToSpeech.QUEUE_FLUSH, null, "Test");
-                }
-
-            }
-        });
     }
 
     @Override
     public void onInit(int status) {
         if (status == TextToSpeech.SUCCESS) {
             tts.setLanguage(Locale.GERMAN);
+        }
+    }
+
+    public void clicked(View view) {
+        if (TextZuSprechen.getText().length() > 0) {
+            tts.speak(TextZuSprechen.getText(), TextToSpeech.QUEUE_FLUSH, null, "Test");
+        } else {
+            tts.speak("Gib einen Text ein", TextToSpeech.QUEUE_FLUSH, null, "Test");
         }
     }
 }
